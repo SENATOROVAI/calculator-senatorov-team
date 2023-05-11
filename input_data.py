b77cb8af07c882d8df4785b914df9812cc058cbd
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-11 23:22:27 +0400

    Update and rename calculator.py to input_data.py

---
+++ input_data.py
@@ -0,0 +1,5 @@
+def get_input_data():
+    num = float(input('Введите число: '))
+    operator = input('Введите операцию: ')
+    num2 = float(input('Введите число: '))
+    return num, operator, num2
