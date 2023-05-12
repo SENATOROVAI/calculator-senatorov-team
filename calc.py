04918736fd1a139ba22281011a3b95a876e329cf
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-13 02:40:44 +0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,7 +4,5 @@ from subtraction import sub
 from multiplication import mult
 
 while True:
-    num = float(input('Введите число: '))
-    operator = input('Введите операцию: ')
-    num2 = float(input('Введите число: '))
+    
     
