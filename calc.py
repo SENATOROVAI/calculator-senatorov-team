af62b375bc3c2b836a4a3bbbef00703aa96eaba6
SergeyMarfin <119563862+SergeyMarfin@users.noreply.github.com>
2023-05-12 21:21:37 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -7,15 +7,4 @@ while True:
     num = float(input('Введите число: '))
     operator = input('Введите операцию: ')
     num2 = float(input('Введите число: '))
-    # ключевое слово:
-#          переменная, переменная, переменная = фукнция()
-        if operator == '+':
-            print('Результат операции:', add(?))
-        elif operator == '-':
-            print('Результат операции:', sub(?))
-        elif operator == '*':
-            print('Результат операции:', mult(?))
-        else:
-            print('Ошибка! Некорректный оператор.')
-    #ключевое слово  ValueError:
-        print(?)
+    
