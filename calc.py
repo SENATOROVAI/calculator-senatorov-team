27bb08dd5a8e2e73fef401ce1542718add26cbeb
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-13 02:47:11 +0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,5 +4,15 @@ from subtraction import sub
 from multiplication import mult
 
 while True:
-    
-    
+    # ключевое слово:
+#          переменная, переменная, переменная = фукнция()
+        if operator == '+':
+            print('Результат операции:', add(?))
+        elif operator == '-':
+            print('Результат операции:', sub(?))
+        elif operator == '*':
+            print('Результат операции:', mult(?))
+        else:
+            print('Ошибка! Некорректный оператор.')
+    #ключевое слово  ValueError:
+        print(?)
