7e610197f8c8bbf5d621b523153f910b5f2d2ca7
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-13 18:35:12 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-# ключевое calc
+import calc
 
 def get_input_data():
-    #num = 
-    #operator =
-    #num2 =
-#     вернуть
+    num = float(inpit(":>"))
+    operator = inpit(":>")
+    num2 = float(inpit(":>"))
+    return num, operator, num2
